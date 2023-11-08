2a4ef1cac0751ccda191049cce0913ea0fe5114a
m7medmagdi <m7med.magdi@gmail.com>
2023-11-08 13:38:48 +0200

    First Commit

---
+++ src/main/java/com/example/simple/auth/with/crud/SimpleAuthWithCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.simple.auth.with.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleAuthWithCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleAuthWithCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/simple/auth/with/crud/SimpleAuthWithCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simple.auth.with.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleAuthWithCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
